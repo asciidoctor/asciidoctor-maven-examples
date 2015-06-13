fd5294415c4184bd43d50a3e6d352e7b6e99c5fd
Jeremie Bresson <jmini@jmini.fr>
2015-06-13 08:44:59 +0200

    Add "java-extension-example"
    Signed-off-by: Jeremie Bresson <jmini@jmini.fr>

---
+++ java-extension-example/asciidoctorj-twitter-extension/src/main/java/org/asciidoctorj/twitter/extension/TwitterMacro.java
@@ -0,0 +1,43 @@
+package org.asciidoctorj.twitter.extension;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.asciidoctor.ast.AbstractBlock;
+import org.asciidoctor.ast.Inline;
+import org.asciidoctor.extension.InlineMacroProcessor;
+
+public class TwitterMacro extends InlineMacroProcessor {
+
+	public TwitterMacro(String macroName, Map<String, Object> config) {
+		super(macroName, config);
+	}
+
+	@Override
+	protected Object process(AbstractBlock parent, String twitterHandle,
+			Map<String, Object> attributes) {
+
+		String twitterLink;
+		String twitterLinkText;
+		if (twitterHandle == null || twitterHandle.isEmpty()) {
+			twitterLink = "http://www.twitter.com/";
+			twitterLinkText = "Twitter";
+		} else {
+			twitterLink = "http://www.twitter.com/" + twitterHandle;
+			// Prepend twitterHandle with @ as text link:
+			twitterLinkText = "@" + twitterHandle;
+		}
+
+		// Define options for an 'anchor' element:
+		Map<String, Object> options = new HashMap<String, Object>();
+		options.put("type", ":link");
+		options.put("target", twitterLink);
+
+		// Create the 'anchor' node:
+		Inline inlineTwitterLink = createInline(parent, "anchor",
+				twitterLinkText, attributes, options);
+
+		// Convert to String value:
+		return inlineTwitterLink.convert();
+	}
+}
