bf120ab3b8030cda26d509fe1088c2630cd9d298
Jeremie Bresson <dev@jmini.fr>
2015-06-30 23:53:25 +0200

    Update asciidoctorj-pdf to "1.5.0-alpha.8"
    - Update the version in the pom files.
    - Add a callout bullet in the asciidoctor-pdf-example java code.
    - Remove outdated warnings in the pom files.
    
    Signed-off-by: Jeremie Bresson <dev@jmini.fr>

--- asciidoctor-pdf-example/src/main/java/example/StringUtils.java
+++ asciidoctor-pdf-example/src/main/java/example/StringUtils.java
@@ -3,7 +3,7 @@ package example;
 public class StringUtils {
     // tag::contains[]
     public boolean contains(String haystack, String needle) {
-        return haystack.contains(needle);
+        return haystack.contains(needle); //<1>
     }
     // end::contains[]
 }
